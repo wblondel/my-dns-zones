cdcb15af28187fcea1cf0927031dbc24cd32fc8e
William Blondel <contact@williamblondel.fr>
2021-12-21 04:24:11 +0100

    Update SSH footprints [SSHFP]

--- dnsconfig.js
+++ dnsconfig.js
@@ -15,24 +15,24 @@ var CONTABO_WEB_SERVER_APEX = [
 
     CAA('@', 'issue', 'letsencrypt.org'),
 
-    SSHFP('@', 1, 1, 'b505f65723fceaa5ad10b5f49983ebf84773f05d'),
-    SSHFP('@', 1, 2, 'd47f836acd7742b3fa768bf1268626744cb1cd1100f0b86c7e1961b9b5b613f4'),
-    SSHFP('@', 3, 1, 'f130474bac0573c90b8b140b828bf15a14b60f28'),
-    SSHFP('@', 3, 2, '1b69ef10cef93da4463817c551c8c8a1b6326909367f53dd121f73e0fd7583d8'),
-    SSHFP('@', 4, 1, 'f69583cf21fd7fe9f9c921a0f529bdfa7db57137'),
-    SSHFP('@', 4, 2, 'c98f122f279af5b6c356c2df5d6d5ab728bee7f0abc2f21661a0450e68e81d3c')
+    SSHFP('@', 1, 1, 'fd45e0017294086203f789a1f76f08aabeedd905'),
+    SSHFP('@', 1, 2, '748b03f80a8f0ac2de539b25700ca5490aa66b195b42c11116245275de9e4b34'),
+    SSHFP('@', 3, 1, '90cb1ed8b5b0df85f9c7960affadb830c01dac4d'),
+    SSHFP('@', 3, 2, '81f50b99333c33280efb03523081b842e08ee13a11232ffe391f9e972f0cdf4e'),
+    SSHFP('@', 4, 1, '7e1caf2f5673ea1d06153b460754001331d3f5fe'),
+    SSHFP('@', 4, 2, 'f1d0e73421f54f148ea04762c717aa6ead3ff4680476b2c4ca6c192f1d19207f')
 ];
 
 var CONTABO_WEB_SERVER_SUBDOMAIN = [
     A('einstein', '161.97.87.85'),
     AAAA('einstein', '2a02:c207:2045:4811::1'),
 
-    SSHFP('einstein', 1, 1, 'b505f65723fceaa5ad10b5f49983ebf84773f05d'),
-    SSHFP('einstein', 1, 2, 'd47f836acd7742b3fa768bf1268626744cb1cd1100f0b86c7e1961b9b5b613f4'),
-    SSHFP('einstein', 3, 1, 'f130474bac0573c90b8b140b828bf15a14b60f28'),
-    SSHFP('einstein', 3, 2, '1b69ef10cef93da4463817c551c8c8a1b6326909367f53dd121f73e0fd7583d8'),
-    SSHFP('einstein', 4, 1, 'f69583cf21fd7fe9f9c921a0f529bdfa7db57137'),
-    SSHFP('einstein', 4, 2, 'c98f122f279af5b6c356c2df5d6d5ab728bee7f0abc2f21661a0450e68e81d3c')
+    SSHFP('einstein', 1, 1, 'fd45e0017294086203f789a1f76f08aabeedd905'),
+    SSHFP('einstein', 1, 2, '748b03f80a8f0ac2de539b25700ca5490aa66b195b42c11116245275de9e4b34'),
+    SSHFP('einstein', 3, 1, '90cb1ed8b5b0df85f9c7960affadb830c01dac4d'),
+    SSHFP('einstein', 3, 2, '81f50b99333c33280efb03523081b842e08ee13a11232ffe391f9e972f0cdf4e'),
+    SSHFP('einstein', 4, 1, '7e1caf2f5673ea1d06153b460754001331d3f5fe'),
+    SSHFP('einstein', 4, 2, 'f1d0e73421f54f148ea04762c717aa6ead3ff4680476b2c4ca6c192f1d19207f')
 ];
 
 var USE_PIGEON_MAIL_SERVER = [
