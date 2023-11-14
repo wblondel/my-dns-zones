88147faa099631f0a224b8601187ac6ec6a81d25
William Gérald Blondel <contact@williamblondel.fr>
2023-11-14 23:59:42 +0800

    Add missing TXT entry

--- dnsconfig.js
+++ dnsconfig.js
@@ -195,5 +195,6 @@ D('williamblondel.fr', REG_NONE, DnsProvider(deSEC),
 
     TXT('@', 'abuseipdb-verification=s5qjw8Yf'),
     TXT('@', 'google-site-verification=AsBRY1djNmUyPuEhPpftRTPafx6j2EtGl6xWFFAsZ0c'),
-    TXT('@', 'keybase-site-verification=C_z29b6Vah7pgfISyRn2Uxd8iq4M-f3Osvr2uWbPg3E')
+    TXT('@', 'keybase-site-verification=C_z29b6Vah7pgfISyRn2Uxd8iq4M-f3Osvr2uWbPg3E'),
+    TXT('@', 'MS=A8E96F2D3E9EA3076A9575F6BC91467586875ECB')
 );
